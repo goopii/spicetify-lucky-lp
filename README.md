a388d75bdafbb3f6cdb10d95b0a89c99fc953286
Malte Henriksson <maltehen132@gmail.com>
2025-05-31 13:09:48 +0200

    simplified code a lot and moved button to bottom left of ui

--- README.md
+++ README.md
@@ -1,37 +1,3 @@
-# LuckyLP - Spicetify Extension
+# random-saved-album-button - Spicetify Extension
 
-LuckyLP is a Spicetify Extension which allows to easily choose random saved album from your library. Just press the "*Get Random Saved Album*" button located in the navigation section of the home or album page.
-
-![luckylp](https://github.com/Akasiek/spicetify-lucky-lp/assets/37307597/6fc31913-2304-4f28-b5c7-fafc6105affc)
-
-## Installation
-
-### Linux/MacOS
-
-```bash
-wget https://raw.githubusercontent.com/Akasiek/spicetify-lucky-lp/main/LuckyLP.js
-mv ./LuckyLP.js ~/.config/spicetify/Extensions/
-spicetify config extensions LuckyLP.js
-spicetify apply
-```
-
-### Windows (Powershell)
-
-```powershell
-wget https://raw.githubusercontent.com/Akasiek/spicetify-lucky-lp/main/LuckyLP.js
-Move-Item -Path .\LuckyLP.js -Destination %appdata%\spicetify\Extensions\
-spicetify config extensions LuckyLP.js
-spicetify apply
-```
-
-## Screeshot
-
-![Screenshot 1](/screenshots/Screenshot_1.png)
-
-![Screenshot 2](/screenshots/Screenshot_2.png)
-
-## Docs
-Check out [Spicetify's docs](https://spicetify.app/docs/development/spicetify-creator/the-basics)!
-
-## Made with Spicetify Creator
-- https://github.com/spicetify/spicetify-creator
+Simple extension to add a button to get a random saved album
--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,19 @@
-import { toggleButton } from "./button";
+import ShuffleIcon from "./shuffleIcon";
+import "./styles.css";
+
+const getRandomSavedAlbum = async (): Promise<void> => {
+  const apiAlbums = await Spicetify.CosmosAsync.get(
+    "https://api.spotify.com/v1/me/albums?limit=1"
+  );
+  const randomIndex = Math.floor(Math.random() * (apiAlbums.total + 1));
+
+  const apiAlbumsOffset = await Spicetify.CosmosAsync.get(
+    `https://api.spotify.com/v1/me/albums?limit=1&offset=${randomIndex - 1}`
+  );
+  const randomAlbum = apiAlbumsOffset.items[0].album;
+
+  Spicetify.Platform.History.push(`/album/${randomAlbum.id}`);
+};
 
 async function main() {
   while (
@@ -9,13 +24,22 @@ async function main() {
     await new Promise((resolve) => setTimeout(resolve, 500));
   }
 
-  // Show button on app load
-  toggleButton(Spicetify.Platform.History.location);
+  const newParentSelector = ".main-nowPlayingWidget-nowPlaying";
+  let newParent = document.querySelector(newParentSelector);
+  while (!newParent) {
+    await new Promise((resolve) => setTimeout(resolve, 100));
+    newParent = document.querySelector(newParentSelector);
+  }
 
-  // Show button depending on navigation
-  Spicetify.Platform.History.listen((location: Location) => {
-    toggleButton(location);
-  });
+  const btn = document.createElement("button");
+  btn.id = "random-saved-album-btn";
+  btn.className = "lucky-lp-button";
+  btn.innerHTML = `<svg role="img">${ShuffleIcon}</svg> Random Album`;
+  btn.addEventListener("click", getRandomSavedAlbum);
+
+  newParent.appendChild(btn);
 }
 
 export default main;
+// .Button-buttonTertiary-medium-iconOnly
+// btn.className = "lucky-lp-button";
